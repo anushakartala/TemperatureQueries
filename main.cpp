25fd88da9aaa5e34bd455d079e07865e534762ee
anushakartala <57739547+anushakartala@users.noreply.github.com>
2020-01-23 13:29:08 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "TemperatureDatabase.h"
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include <sstream>
+
+// You do not need to edit this code
+
+int main(int argc, char** argv)
+{
+	if (argc < 3)
+	{
+		cout << "Usage: " << argv[0] << " data_file query_file" << endl;
+		return 1;
+	}
+	else
+	{
+		TemperatureDatabase database;
+		database.loadData(argv[1]); // Do in Part 1
+		database.performQuery(argv[2]); // Do in Part 2
+	}
+
+}
